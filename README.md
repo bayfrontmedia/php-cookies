d86510bd4e3590688d7d49e9806944985d6d463c
John Robinson <john@bayfrontmedia.com>
2020-07-27 14:30:06 -0400

    Initial release

---
+++ README.md
@@ -0,0 +1,166 @@
+## PHP cookies
+
+Helper class to easily and safely work with cookies.
+
+- [License](#license)
+- [Author](#author)
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Usage](#usage)
+
+## License
+
+This project is open source and available under the [MIT License](https://github.com/bayfrontmedia/php-array-helpers/blob/master/LICENSE).
+
+## Author
+
+John Robinson, [Bayfront Media](https://www.bayfrontmedia.com)
+
+## Requirements
+
+* PHP > 7.1.0
+
+## Installation
+
+```
+composer require bayfrontmedia/php-cookies
+```
+
+## Usage
+
+- [get](#get)
+- [has](#has)
+- [set](#set)
+- [forget](#forget)
+- [forgetAll](#forgetall)
+
+<hr />
+
+### get
+
+**Description:**
+
+Returns value of single `$_COOKIE` array key or entire array, with optional default value.
+
+**Parameters:**
+
+- `$key = NULL` (string)
+- `$default = NULL` (mixed): Default value to return if the array key is not found
+
+**Returns:**
+
+- (mixed)
+
+**Example:**
+
+```
+use Bayfront\Cookies\Cookie;
+
+print_r(Cookie::get());
+```
+
+<hr />
+
+### has
+
+**Description:**
+
+Checks if `$_COOKIE` array key exists.
+
+**Parameters:**
+
+- `$key` (string)
+
+**Returns:**
+
+- (bool)
+
+**Example:**
+
+```
+use Bayfront\Cookies\Cookie;
+
+if (Cookie::has('cart_id')) {
+    // Do something
+}
+```
+
+<hr />
+
+### set
+
+**Description:**
+
+Creates a cookie.
+
+See: [https://www.php.net/manual/en/function.setcookie.php](https://www.php.net/manual/en/function.setcookie.php)
+
+**Parameters:**
+
+- `$name` (string): Cookie name
+- `$value` (string): Cookie value
+- `$minutes = 0` (int): Minutes from now until the cookie expires
+- `$path = '/'` (string): Path on the server in which the cookie will be available
+- `$domain = ''` (string): Domain/subdomain that the cookie is available to
+- `$secure = true` (bool): Transmit the cookie only over a secure https connection
+- `$http_only = true` (bool): Accessible only through the http protocol
+
+**Returns:**
+
+- (bool)
+
+**Example:**
+
+```
+use Bayfront\Cookies\Cookie;
+
+Cookie::set('cart_id', 'abc123', 60);
+```
+
+<hr />
+
+### forget
+
+**Description:**
+
+Removes validity of cookie.
+
+**Parameters:**
+
+- `$name` (string)
+
+**Returns:**
+
+- (void)
+
+**Example:**
+
+```
+use Bayfront\Cookies\Cookie;
+
+Cookie::forget('cart_id');
+```
+
+<hr />
+
+### forgetAll
+
+**Description:**
+
+Removes the validity of all cookies.
+
+**Parameters:**
+
+- None
+
+**Returns:**
+
+- (void)
+
+**Example:**
+
+```
+use Bayfront\Cookies\Cookie;
+
+Cookie::forgetAll();
+```
---
+++ src/Cookie.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * Helper class to easily and safely work with cookies
+ *
+ * @version     1.0.0
+ * @link        https://github.com/bayfrontmedia/php-cookies
+ * @license     MIT https://opensource.org/licenses/MIT
+ * @copyright   2020 Bayfront Media https://www.bayfrontmedia.com
+ * @author      John Robinson <john@bayfrontmedia.com>
+ */
+
+namespace Bayfront\Cookies;
+
+class Cookie
+{
+
+    /**
+     * Returns value of single $_COOKIE array key or entire array, with optional default value
+     *
+     * @param string $key
+     * @param mixed $default (Default value to return if the array key is not found)
+     *
+     * @return mixed
+     */
+
+    public static function get(string $key = NULL, $default = NULL)
+    {
+
+        if (NULL === $key) { // Return the entire array
+
+            return $_COOKIE;
+
+        }
+
+        // Return a specific key
+
+        if (isset($_COOKIE[$key])) {
+
+            return $_COOKIE[$key];
+
+        }
+
+        return $default;
+
+    }
+
+    /**
+     * Checks if $_COOKIE array key exists
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
+
+    public static function has(string $key): bool
+    {
+        return (self::get($key)) ? true : false;
+    }
+
+    /**
+     * Creates a cookie
+     *
+     * @param string $name (Cookie name)
+     * @param string $value (Cookie value)
+     * @param int $minutes (Minutes from now until the cookie expires)
+     * @param string $path (Path on the server in which the cookie will be available)
+     * @param string $domain (Domain/subdomain that the cookie is available to)
+     * @param bool $secure (Transmit the cookie only over a secure https connection)
+     * @param bool $http_only (Accessible only through the http protocol)
+     *
+     * @return bool
+     */
+
+    public static function set(string $name, string $value, int $minutes = 0, string $path = '/', string $domain = '', bool $secure = true, bool $http_only = true): bool
+    {
+
+        // -1 = Delete, 0 = Expire at end of session, > 0 = Minutes from now
+
+        if ($minutes < 0) {
+
+            $time = 1;
+
+        } else if ($minutes == 0) {
+
+            $time = 0;
+
+        } else {
+
+            $time = time() + 60 * $minutes;
+
+        }
+
+        $set = setcookie($name, $value, $time, $path, $domain, $secure, $http_only);
+
+        if ($set) {
+
+            $_COOKIE[$name] = $value; // Make available without reloading page
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    /**
+     * Removes validity of cookie
+     *
+     * @param string $name
+     *
+     * @return void
+     */
+
+    public static function forget(string $name): void
+    {
+
+        self::set($name, false, -1); // Expire in browser
+
+        unset($_COOKIE[$name]); // Remove from script
+
+    }
+
+    /**
+     * Removes the validity of all cookies
+     *
+     * @return void
+     */
+
+    public static function forgetAll(): void
+    {
+
+        $cookies = self::get();
+
+        if (is_array($cookies)) {
+
+            foreach ($cookies as $cookie) {
+
+                self::forget($cookie);
+
+            }
+
+        }
+
+    }
+
+}
